da2f25f7146d54a82b93524d4269e2abcc02a921
Austin Wolfe <wolfe_austin@yahoo.com>
2018-04-23 11:54:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file
+function findMatching(array, string){
+  let arrayToRet = [];
+  for(const key in array){
+    if (array[key].toUpperCase() === string.toUpperCase()){
+      arrayToRet.push(array[key]);
+    }
+  }
+  return arrayToRet;
+}
+function fuzzyMatch(array, string){
+  let arrayToRet = [];
+  for(const key in array){
+    if (array[key][0] === string[0]){
+      arrayToRet.push(array[key]);
+    }
+  }
+  return arrayToRet;
+}
+
+function matchName(array, string){
+  let arrayToRet = [];
+  for(const key in array){
+    if (array[key].name === string){
+      arrayToRet.push(array[key]);
+    }
+  }
+  return arrayToRet;
+}
