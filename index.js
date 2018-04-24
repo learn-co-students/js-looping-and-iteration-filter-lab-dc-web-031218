6b7873c206878b31beba2a869e4ea8bc0a0c8160
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-04-24 18:33:51 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file
+function findMatching(list, name) {
+  a = list.filter(function(n){
+      return n.toLowerCase() === name.toLowerCase();
+  });
+  return a
+}
+
+function fuzzyMatch(list, namePart) {
+  // returns a driver if beginning provided letters match
+  const section = namePart.length;
+  a = list.filter(function(n){
+    nSlice = n.slice(0, section);
+    return nSlice.toLowerCase() === namePart.toLowerCase();
+  });
+  return a
+}
+
+function matchName(list, name) {
+  a = list.filter(function(n){
+      return n.name.toLowerCase() === name.toLowerCase();
+  });
+  return a
+}
